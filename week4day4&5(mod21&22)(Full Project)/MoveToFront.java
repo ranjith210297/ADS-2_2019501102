8772c583d5203a5b5431802ee2b4c73e1c43e085
ranjith210297 <ranjeeth2197@msitprogram.net>
2020-02-10 08:59:06 +0530

    week4day5(mod22)+Full Project-98%

---
+++ week4day4&5(mod21&22)(Full Project)/MoveToFront.java
@@ -0,0 +1,74 @@
+
+/**
+ * class that implements move to front .
+ * @author k. Ranjith kumar 
+ * @reference Bob Sedgewick
+ */
+public class MoveToFront {
+    private static final int R = 256;
+
+    /**
+     * method for applying encoding inputArr and output.
+     * 
+     */
+    public static void encode() {
+        char[] sequenceArr = new char[R];
+        for (int i = 0; i < R; i++) {
+            sequenceArr[i] = (char) i;
+        }
+
+        String s = BinaryStdIn.readString();
+        char[] inputArr = s.toCharArray();
+
+        for (int i = 0; i < inputArr.length; i++) {
+            char in = inputArr[i];
+
+            int out;
+            for (out = 0; out < sequenceArr.length; out++) {
+                if (sequenceArr[out] == in) {
+                    break;
+                }
+            }
+            assert out < sequenceArr.length;
+
+            BinaryStdOut.write((char) out);
+
+            System.arraycopy(sequenceArr, 0, sequenceArr, 1, out);
+            sequenceArr[0] = in;
+        }
+
+        BinaryStdOut.close();
+    }
+
+
+    /**
+     * method for applying decoding iput and output.
+     */
+    public static void decode() {
+        char[] sequenceArr = new char[R];
+        for (int i = 0; i < R; i++) {
+            sequenceArr[i] = (char) i;
+        }
+
+        String s = BinaryStdIn.readString();
+        char[] inputArr = s.toCharArray();
+
+        for (int i = 0; i < inputArr.length; i++) {
+            int in = inputArr[i];
+            char ch = sequenceArr[in];
+
+            BinaryStdOut.write(ch);
+
+            System.arraycopy(sequenceArr, 0, sequenceArr, 1, in);
+            sequenceArr[0] = ch;
+        }
+
+        BinaryStdOut.close();
+    }
+
+    public static void main(String[] args) {
+        if      (args[0].equals("-")) encode();
+        else if (args[0].equals("+")) decode();
+        else throw new IllegalArgumentException("Illegal command line argument");
+    }
+}
